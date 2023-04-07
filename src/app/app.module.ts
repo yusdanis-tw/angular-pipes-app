0dfbab388a95c6595a8732cb89438f84fe906926
Yusdanis Feus <yusdanis.feus@thoughtworks.com>
2023-04-07 12:29:58 -0500

    Change default locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import { LOCALE_ID, NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
@@ -8,10 +8,20 @@ import { SharedModule } from './shared/shared.module'
 import { AppRouterModule } from './app-router.module'
 import { VentasModule } from './ventas/ventas.module'
 
+// Cambiar el locale de la app
+import localeES from '@angular/common/locales/es'
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localeES)
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es',
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
