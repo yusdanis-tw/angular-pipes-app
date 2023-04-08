b05c5a558c74d76bf6cd933897f0bff045a16bcb
Yusdanis Feus <yusdanis.feus@thoughtworks.com>
2023-04-08 11:51:23 -0500

    i18nPural Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -6,10 +6,19 @@ import { Component } from '@angular/core'
   styles: [],
 })
 export class NoComunesComponent {
+  // i18nSelect
   nombre: string = 'Yusdanis'
   genero: string = 'masculino'
   invitacionMapa = {
     masculino: 'invitarlo',
     femenino: 'invitarla',
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan']
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    other: 'tenemos # clientes esperando.',
   }
 }
