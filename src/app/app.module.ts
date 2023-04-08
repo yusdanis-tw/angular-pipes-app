1f86621cc24f7ad5fde18883026cee0290f40d63
Yusdanis Feus <yusdanis.feus@thoughtworks.com>
2023-04-08 11:35:42 -0500

    promeNg - Fieldset

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { LOCALE_ID, NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component'
 
@@ -17,7 +18,13 @@ registerLocaleData(localeFr)
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, AppRouterModule, VentasModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AppRouterModule,
+    VentasModule,
+    BrowserAnimationsModule,
+  ],
   providers: [
     {
       provide: LOCALE_ID,
