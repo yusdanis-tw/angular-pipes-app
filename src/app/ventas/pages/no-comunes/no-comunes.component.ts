eff0f43b8d6847fa86187ecf23b8b9184ac3836f
Yusdanis Feus <yusdanis.feus@thoughtworks.com>
2023-04-09 14:17:33 -0500

    Async Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { interval } from 'rxjs'
 
 @Component({
   selector: 'app-no-comunes',
@@ -53,4 +54,12 @@ export class NoComunesComponent {
       vuela: false,
     },
   ]
+
+  // Async Pipe
+  miObservable = interval(1000)
+  valorPrimesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa')
+    }, 3500)
+  })
 }
